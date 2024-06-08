c4f8c37192ecd9b045f235737ca66c547fb65dae
rcomunica <julir2772@gmail.com>
2024-06-08 16:32:38 -0500

    Correction #12 | Add video about event

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,5 +4,12 @@
             <x-stats-flight :booking="$userBooking" :countPilotBookings="$countPilotBookings"
                 :countAtcBookings="$countAtcBookings" :principalHub="$principalHub" />
         </div>
+        <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-5 flex items-center flex-col">
+            <h3 class="text-3xl mb-8 text-center font-bold text-[#3C55AC]">About this event</h3>
+            <iframe width="853" height="480" src="https://www.youtube.com/embed/g5VjIuk8rJc"
+                title="Colombia Real Dispatch 2024" frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        </div>
     </div>
 </div>
